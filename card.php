c73d577d67a9b25430b1620582486a7f1eb832d1
Ju/TroisYaourts <julien@troisyaourts.com>
2016-01-19 12:59:16 +0100

    TODO

--- card.php
+++ card.php
@@ -58,10 +58,55 @@ class Deck{
   }
 }
 
-$d = new Deck();
-list($carte) = $d->shuffle()->deal(1);
+class Player{
+  protected $hand; //sert à stocker les cartes ue le joueur possède
+  protected $pseudo;
 
-echo $carte;
+  public function __construct($toto){
+      $this->hand = [];
+      $this->pseudo = $toto;
+  }
+
+  public function take($cards){
+    //TODO : récupère les cartes passées en paramètre
+    //et les place dans $this->hand
+  }
+
+  public function getHandValue(){
+    //TODO : compter et returner la somme des valeurs de la main de la personne
+    return 22;
+  }
+
+}
+
+class Bank extends Player{
+  public function __construct(){
+      parent::__construct("Banque");
+  }
+}
+
+//SCENARIO 1
+
+$deck = new Deck();
+$deck->shuffle();
+$bank = new Bank();
+
+$bank->take($deck->deal(2)); //tire 2 cartes du deck, la banque les prends
+
+while( $bank->getHandValue() < 17){
+  $bank->take($deck->deal(1)); 
+}
+
+if($bank->getHandValue() > 21){
+  echo "La banque perd";
+}else{
+  echo "La banque a ".$bank->getHandValue();
+}
+
+
+
+
+
 
 
 
